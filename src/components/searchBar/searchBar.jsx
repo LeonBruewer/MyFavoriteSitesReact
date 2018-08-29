fe0de4e8f1e5bfebe6cada454a0ac50918f01567
Leon Brüwer <leon.bruewer@web.de>
2018-08-29 15:35:39 +0200

    added searchterm eventhandling

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -4,12 +4,35 @@ import './accordionSearchBar.scss';
 export default class SearchBar extends React.Component {
     constructor(props) {
         super(props);
+        this.timeout;
         this.default = this.props.default;
+        this.state = {
+            value: '',
+            searchTerm: ''
+        };
+
+        this.onChangeAction = props.action;
+    }
+
+    onInputChange = (e) => {
+        let newValue = e.target.value;
+        this.setState({value: newValue});
+
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout( () => {
+            this.setState({searchTerm: newValue});
+            let searchTerm = this.default;
+            
+            if(this.state.searchTerm !== '')
+                searchTerm = this.state.searchTerm;
+
+            this.onChangeAction(searchTerm);
+        }, 500);
     }
 
     render = () =>
     <div id="searchBar" className="Suche Suche--accordion">
-        <input id="searchBar--searchTerm" type="text" placeholder="Suche" value="" />
+        <input id="searchBar--searchTerm" type="text" placeholder="Suche" onChange={this.onInputChange} value={this.state.value} />
         <label><i className="fa fa-search"></i></label>
     </div>;
 }
--- src/components/siteList/siteList.jsx
+++ src/components/siteList/siteList.jsx
@@ -6,13 +6,17 @@ export default class SiteList extends React.Component {
         super(props);
     }
 
+    createList = (searchTerm) => {
+        console.log(searchTerm);
+    }
+
     render = () =>
     <div className="accordion accordion--open" data-group="mfs" id="sitesAccordion">
-            <div className="accordion__head accordion__head--search">Sites<SearchBar default="chayns"/></div>
-            <div className="accordion__body">
-                <div className="accordion__content">
-                    <div id="siteList"></div>
-                </div>
+        <div className="accordion__head accordion__head--search">Sites<SearchBar default="chayns" action={this.createList}/></div>
+        <div className="accordion__body">
+            <div className="accordion__content">
+                <div id="siteList"></div>
             </div>
-        </div>;
+        </div>
+    </div>;
 }
