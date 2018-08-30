83c5347a425d24efbcb7cbf12c9f0eb0dfd072d8
Leon Brüwer <leon.bruewer@web.de>
2018-08-30 15:34:22 +0200

    deleted old template code

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,52 +6,15 @@ import AddSite from './addSite/addSite';
 export default class Content extends React.Component {
     constructor(props) {
         super(props);
-        /**
-         * Apply an array containing selected users to the content
-         * It can be filled by using the PersonFinder-Component which is Visible in Adminmode
-         */
+
         this.state = {
-            users: []
+            
         };
-        console.log(this.props.formOpen);
     }
 
-    /**
-     * Push´s an user object to the state 'users' and refresh's the react component 'UserList'
-     * By Calling setState the renderfunction ist called automatically
-     */
-    addUser = (user) => {
-        const { users } = this.state;
-        if (users.find(u => u.userId === user.userId)) {
-            return;
-        }
-
-        this.setState({
-            users: [...users, user]
-        });
-    };
-
-    /**
-     *  locally save the state `users` and filter the array by a userId
-     *  By Calling setState the renderfunction ist called automatically
-     */
-    removeUser = (userId) => {
-        const { users } = this.state;
-        this.setState({
-            users: users.filter(u => u.userId !== userId)
-        });
-    };
-
     render() {
-        const { users } = this.state;
         return (
             <div className="tapp__content content">
-                {/**
-                 * Everything defined as a child of Mode is only visible for users
-                 * in the UAC-Group 1
-                 *
-                 * The UAC-Group 1 is defined for chayns-Manager
-                 */}
                  <SiteList open={!this.props.formOpen}/>
                  <AddSite open={this.props.formOpen} title="Seite hinzufügen"/>
             </div>
--- src/components/siteList/siteList.jsx
+++ src/components/siteList/siteList.jsx
@@ -54,17 +54,21 @@ export default class SiteList extends React.Component {
 
     createListItems = (title, id) => <ListItem title={title} description={id} key={id}/>;
 
-    render = () =>{ console.log(`open: ${this.props.open}`);
-    return <div className={`accordion ${this.state.open ? 'accordion--open' : ''}`} data-group="mfs" id="sitesAccordion">
-        <div className="accordion__head accordion__head--search">Sites<SearchBar default="chayns" action={this.createList}/></div>
-        <div className="accordion__body">
-            <div className="accordion__content">
-                <div id="siteList">
-                    {this.state.list}
-                    <ShowMore display={this.state.showMoreStyleDisplay} onClick={this.showMore}/>
+    render = () =>
+    (
+        <div className={`accordion ${this.state.open ? 'accordion--open' : ''}`} data-group="mfs" id="sitesAccordion">
+            <div className="accordion__head accordion__head--search">Sites<SearchBar default="chayns" action={this.createList}/></div>
+            <div className="accordion__body">
+                <div className="accordion__content">
+                    <div id="siteList">
+                        {this.state.list}
+                        <ShowMore
+                            display={this.state.showMoreStyleDisplay}
+                            onClick={this.showMore}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>;
-    }
+    );
 }
